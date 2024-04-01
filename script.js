7eeafeae05a31757f89918edbbb55dd2641be0d9
Wellington <welltecnic@hotmail.com>
2024-04-01 09:45:38 -0300

    Criando o script da classificação torneio

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function calc(){
 //DECLARANDO AS VARIAVEIS 
 let num1 =parseInt(document.getElementById('num1').value); 
 let num2 =parseInt(document.getElementById('num2').value);
-
 document.getElementById('resultado').innerHTML = num1 * num2;
 
+}
+
+function inserir(){
+    let numero= document.getElementById("posicao").value -1;
+    let novo= document.getElementById("novo").value;
+    document.getElementsByClassName("time")[numero].innerHTML = novo
 }
